ef3da21c86831f4284fbe30d406db023ec90c673
laska-adief <laskaadief.la@gmail.com>
2025-02-17 21:45:53 +0700

    create Publisher entity

---
+++ src/main/java/com/solicitut/learning_jpa/domain/Publisher.java
@@ -0,0 +1,77 @@
+package com.solicitut.learning_jpa.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Publisher {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
+  private String address;
+  private String phone;
+  private String email;
+  private String website;
+
+  @OneToMany(mappedBy = "publisher")
+  private Set<Book> books;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getWebsite() {
+    return website;
+  }
+
+  public void setWebsite(String website) {
+    this.website = website;
+  }
+
+  public Set<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(Set<Book> books) {
+    this.books = books;
+  }
+}
